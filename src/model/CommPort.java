95f1b971e13eaed904bd8619d5f2af26708c6c4a
[AlySALL] <[aly.sall@viacesi.fr]>
2017-11-20 10:14:16 +0100

    ajout des lib RXTX

--- src/model/CommPort.java
+++ src/model/CommPort.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.util.Enumeration;
+
+import gnu.io.CommPortIdentifier;
+
 public class CommPort {
 
+	public static void main(String[] args) {
+		System.out.println(" Program Started!!! ");
+
+		CommPortIdentifier serialPortId;
+
+		Enumeration enumComm;
+
+		enumComm = CommPortIdentifier.getPortIdentifiers();
+
+		while (enumComm.hasMoreElements()) {
+			serialPortId = (CommPortIdentifier) enumComm.nextElement();
+			if (serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				System.out.println(serialPortId.getName());
+			}
+		}
+
+		System.out.println("Program Finished Sucessfully ");
+	}
 }
