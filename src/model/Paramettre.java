fd11f61469210489e6877df0629ea5d919e34944
[AlySALL] <[aly.sall@viacesi.fr]>
2017-11-22 11:05:20 +0100

    Class paramettre

--- src/model/Paramettre.java
+++ src/model/Paramettre.java
@@ -2,53 +2,10 @@ package model;
 
 public class Paramettre {
 
-	private Double Temperature;
-	private Boolean Condensation;
-	private Double Humidity;
+	private int Consigne;
 	
-	
-	
-
 	public Paramettre(double temperature, double Humidity) {
-		this.setHumidity(Humidity);
-		this.Temperature=Temperature;
-		this.Condensation=Condensation;
 		
-		Paramettre condensation = new Paramettre(20.0,25.0);
-		System.out.println(condensation.equals(Condensation));
 	}
 
-
-
-	public Double getTemperature() {
-		return Temperature;
 	}
-
-
-	public void setTemperature(Double temperature) {
-		Temperature = temperature;
-	}
-
-
-	public Boolean getCondensation() {
-		return Condensation;
-	}
-
-
-	public void setCondensation(Boolean condensation) {
-		Condensation = condensation;
-	}
-
-
-
-	public Double getHumidity() {
-		return Humidity;
-	}
-
-
-
-	public void setHumidity(Double humidity) {
-		Humidity = humidity;
-	}
-}
-	
