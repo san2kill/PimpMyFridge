953a6995b987dec50b53d8dbf3a5adb7e1af1be4
[AlySALL] <[aly.sall@viacesi.fr]>
2017-11-22 16:34:55 +0100

    main

--- src/main/main.java
+++ src/main/main.java
@@ -13,14 +13,13 @@ public  class main {
     public static void main(final String[] args) {
     	
     	Serial temp = new Serial();
-    	Serial main = new Serial();
+    	
     	System.out.println(temp.getTemperature_Pelier());
         final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
         	 controller.start();
         	 
         	
-        	 
-          
+    
         	 
     }
 
