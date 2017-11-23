a6f3f67178165a4922f717e5a16714238c5a4117
[AlySALL] <[aly.sall@viacesi.fr]>
2017-11-23 12:09:34 +0100

    Modification de la classe main et du constructeur Serial

--- src/main/main.java
+++ src/main/main.java
@@ -1,27 +1,20 @@
 package main;
 
-
-
 import controller.ControllerFacade;
 import model.ModelFacade;
 import model.Serial;
 import view.ViewFacade;
 
+public class main {
 
-public  class main {
+	public static void main(final String[] args) {
 
-    public static void main(final String[] args) {
-    	
-    	Serial temp = new Serial();
-    	
-    	System.out.println("temp1="+temp.getTemperature_Pelier());
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-        	 controller.start();
-        
-        	 
-        	
-   
-        	 
-    }
+		Serial temp = new Serial();
+
+		System.out.println("temp1=" + temp.getTemperature_Pelier());
+		final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+		controller.start();
+
+	}
 
 }
